db075300bb3737cc2d4a435040e90c45ddaf5c06
Suprith Satish <73060086+Suprith-Satish@users.noreply.github.com>
2024-08-04 17:28:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
 
 # Write directly to the app
 st.title("My parents new healthy diner :cup_with_straw:")
@@ -38,6 +39,11 @@ if ingredients_list:
         session.sql(my_insert_stmt).collect()
 
         st.success('Your Smoothie is ordered'+", "+name_on_order)
+
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
 
 
 
